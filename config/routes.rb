395fb2383a4ccd3f941a2023b5d6b28722894c1e
goza-11 <muddy.brain29@gmail.com>
2017-03-16 03:28:51 +0900

    削除処理追加。　検索処理作成中。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :actors, only: [:index, :destroy, :edit, :new, :create, :update]
+  root 'actors#index'
+  resources :actors, only: [:index, :destroy, :edit, :new, :create, :update] do
+    collection {get 'search'}
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
