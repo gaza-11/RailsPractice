3b4918237c0f674b93f8fb91419408443bf7bc2d
goza-11 <muddy.brain29@gmail.com>
2017-03-24 22:51:01 +0900

    FirstNameとLastNameを同時指定した検索ができないバグを修正

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,7 +2,7 @@ class Actor < ApplicationRecord
   class << self
     def search (first_name, last_name)
       if first_name.present? && last_name.present?
-        @actors = where 'first_name like ?'.where 'last_name like ?', "%#{first_name}%", "%#{last_name}%"
+        @actors = where('first_name like ?', "%#{first_name}%").where( 'last_name like ?', "%#{last_name}%")
       elsif first_name.present?
         @actors = where 'first_name like ?', "%#{first_name}%"
       elsif last_name.present?
