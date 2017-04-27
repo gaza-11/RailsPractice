27fcf84261d98aa0ccb11af0a191369502ec9cb5
goza-11 <muddy.brain29@gmail.com>
2017-04-27 21:17:13 +0900

    rubocopの指摘反映。

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Actor.create(:first_name => 'カツオ', :last_name => '磯野')
-Actor.create(:first_name => 'タラオ', :last_name => '磯野')
-Actor.create(:first_name => '波平', :last_name => '磯野')
+Actor.create(first_name: 'カツオ', last_name: '磯野')
+Actor.create(first_name: 'タラオ', last_name: '磯野')
+Actor.create(first_name: '波平', last_name: '磯野')
