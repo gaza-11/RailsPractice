9c4f140417a3fbf00a0f8ea9f79bd8c51f7e9785
goza-11 <muddy.brain29@gmail.com>
2017-05-16 23:55:21 +0900

    modelのテストコードを追加・修正。

--- test/factories/actors.rb
+++ test/factories/actors.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :actor do
     sequence(:first_name) { |n| "Test#{n}" }
     sequence(:last_name) { |n| "User#{n}" }
+    birthday { 20.years.ago }
   end
 end
--- test/models/actor_test.rb
+++ test/models/actor_test.rb
@@ -4,7 +4,32 @@ class ActorTest < ActiveSupport::TestCase
   test'actor can create' do
     actor = FactoryGirl.create(:actor)
     assert_not_nil actor
-    # assert_equal 'Test1', actor.first_name
-    # assert_equal 'User1', actor.last_name
+  end
+
+  test 'actor not nil validate check' do
+    actor = FactoryGirl.create(:actor)
+    assert_not_nil actor.first_name
+    assert_not_nil actor.last_name
+
+    actor.first_name = nil
+    actor.last_name = nil
+    assert actor.invalid?
+    assert actor.errors.include? :first_name
+    assert actor.errors.include? :last_name
+  end
+
+  test 'actor length validate check' do
+    actor = FactoryGirl.build :actor
+    actor.first_name = ''
+    actor.last_name = ''
+    assert actor.invalid?
+    assert actor.errors.include? :first_name
+    assert actor.errors.include? :last_name
+
+    actor.first_name = '123456789012345678901'
+    actor.last_name = '123456789012345678901'
+    assert actor.invalid?
+    assert actor.errors.include? :first_name
+    assert actor.errors.include? :last_name
   end
 end
