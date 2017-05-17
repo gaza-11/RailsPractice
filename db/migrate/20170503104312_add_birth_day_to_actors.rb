bb4579bae4079a583252be389fc6e3268bb25a27
goza-11 <muddy.brain29@gmail.com>
2017-05-17 20:46:43 +0900

    birthdayカラムのMigrateファイルをUpとDownメソッドに変更。

--- db/migrate/20170503104312_add_birth_day_to_actors.rb
+++ db/migrate/20170503104312_add_birth_day_to_actors.rb
@@ -1,7 +1,13 @@
 class AddBirthDayToActors < ActiveRecord::Migration[5.0]
-  def change
+  def up
     add_column :actors, :birthday, :date
     change_column :actors, :first_name, :string, limit: 20, null: false
     change_column :actors, :last_name, :string, limit: 20, null: false
+  end
+
+  def down
+    remove_column :actors, :birthday
+    change_column :actors, :first_name, :string, null: false
+    change_column :actors, :last_name, :string, null: false
   end
 end
