fd81abbaead34c777a5e90cce2a8d7613b65b72f
goza-11 <muddy.brain29@gmail.com>
2017-05-31 21:44:46 +0900

    Controllerの全メソッドのテストを追加。

--- test/controllers/actors_controller_test.rb
+++ test/controllers/actors_controller_test.rb
@@ -4,6 +4,9 @@ class ActorsControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
     get '/actors/'
     assert_response :success
+
+    get '/'
+    assert_response :success
   end
 
   test 'should get search' do
@@ -12,7 +15,7 @@ class ActorsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get new' do
-    get '/actors/new/'
+    get '/actors/new'
     assert_response :success
   end
 
@@ -21,4 +24,30 @@ class ActorsControllerTest < ActionDispatch::IntegrationTest
     get "/actors/#{actor.id}/edit/"
     assert_response :success
   end
+
+  test 'should delete' do
+    actor = FactoryGirl.create(:actor)
+    delete "/actors/#{actor.id}"
+    assert_redirected_to '/'
+  end
+
+  test 'should create'do
+    post '/actors/', params: { actor: { first_name: 'FirstName', last_name: 'LastName', birthday: '2001/1/1' } }
+    assert_redirected_to '/'
+  end
+
+  test 'should update(patch)' do
+    actor = FactoryGirl.create(:actor)
+    patch "/actors/#{actor.id}", params: { actor: { first_name: 'Updated' } }
+    assert_redirected_to '/'
+    assert_equal 'Updated', (Actor.find actor.id).first_name
+  end
+
+  test 'should update(put)' do
+    actor = FactoryGirl.create(:actor)
+    put "/actors/#{actor.id}", params: { actor: { first_name: 'Updated' } }
+    assert_redirected_to '/'
+    assert_equal 'Updated', (Actor.find actor.id).first_name
+  end
 end
+
