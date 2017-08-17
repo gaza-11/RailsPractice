0e05a27195e70302e0a96817458c08da33fd6649
goza-11 <muddy.brain29@gmail.com>
2017-08-17 21:02:20 +0900

    Helperメソッドをhelpersディレクトリ内のファイルに移動。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,10 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def current_member
-    @current_member ||= Member.find_by id: session[:member_id] if session[:member_id]
-  end
-
   def login_required
     return if current_member
 
     flash.alert = 'その画面・機能の利用にはログインが必要です。'
     redirect_to :root
   end
-
-  helper_method :current_member
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def current_member
+    @current_member ||= Member.find_by id: session[:member_id] if session[:member_id]
+  end
 end
